3813ce3ff78b5b20143f901c1290c2b8abdf95e2
shinjjune <sjh8236@naver.com>
2019-11-17 17:58:34 +0900

    spring getMethod

--- src/main/java/com/example/firstspring/controller/GetController.java
+++ src/main/java/com/example/firstspring/controller/GetController.java
@@ -1,8 +1,7 @@
 package com.example.firstspring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.firstspring.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api") //localhost:8080/api
@@ -12,5 +11,23 @@ public class GetController{
     public String getRequest(){
 
         return "Hi getMethod";
+    }
+    @GetMapping("/getParameter") // localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam(name = "password") String pwd) {
+        String password = "bbbb";
+
+        System.out.println("id :"+ id);
+        System.out.println("pwd :"+ pwd);
+
+        return id+pwd;
+    }
+
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam) {
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        return searchParam;
     }
 }
