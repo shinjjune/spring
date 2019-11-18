4db96fd3fda3fd7b545a5e5fcfcd1950cee20302
shinjjune <sjh8236@naver.com>
2019-11-18 19:43:40 +0900

    spring mysql, JPA

---
+++ src/main/java/com/example/firstspring/model/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.firstspring.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Entity
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createAt;
+
+    private String createBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+}
